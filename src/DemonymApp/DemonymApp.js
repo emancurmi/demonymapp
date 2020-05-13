1212444f39a3e8b53c7db0117bc99fd5a284c9a4
Eman Curmi <emancurmi@gmail.com>
2020-05-13 14:20:10 -0400

    finalized project

--- src/DemonymApp/DemonymApp.js
+++ src/DemonymApp/DemonymApp.js
@@ -6,6 +6,7 @@ import CountrySelector from '../CountrySelector/CountrySelector';
 
 
 class DemonymApp extends Component {
+
     constructor(props) {
         super(props);
         this.state = {
@@ -15,27 +16,51 @@ class DemonymApp extends Component {
     }
 
     componentDidMount() {
-        fetch('https://country.register.gov.uk/records.json?page-size=5000')
+        fetch('https://country.register.gov.uk/recordsWRONG.json?page-size=5000')
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Something went wrong, please try again later.')
+                }
+                return response;
+            })
             .then(response => response.json())
             .then(data => {
                 const countries = Object.keys(data)
                     .map(key => data[key].item[0]);
                 this.setState({
-                    countries
+                    countries,
+                    error: null
+                });
+            })
+            .catch(err => {
+                this.setState({
+                    error: err.message
                 });
             });
     }
 
+    setSelected(selected) {
+        this.setState({
+            selected
+        });
+    }
 
     render() {
         const demon = this.state.selected
             ? <Demonym name={this.state.selected['citizen-names']} country={this.state.selected.name} />
             : <div className="demonym_app__placeholder">Select a country above</div>;
 
+        const error = this.state.error
+            ? <div className="demonym_app__error">{this.state.error}</div>
+            : "";
+
         return (
             <div className="demonym_app">
-                <CountrySelector countries={[{ name: "Barbados" }, { name: "Bahrain" }]} />
-                <Demonym name="Barbadian" country="Barbados" />
+                {error}
+                <CountrySelector
+                    countries={this.state.countries}
+                    changeHandler={selected => this.setSelected(selected)} />
+                {demon}
             </div>
         );
     }
